b691a8ab64c03ce353758a54fec2be0859be3d08
Alexander <alexanderbenitezrivpla97@gmail.com>
2026-07-02 19:14:59 -0300

    feat: agrega Gate de admin (EM-7) y corrige ruta de logout faltante

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,10 +11,11 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    'name',
+    'email',
+    'password',
+    'role',
+];
 
     protected $hidden = [
         'password',
